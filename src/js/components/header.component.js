fea802d25be453ec14376b1c174cb94c192158bd
Nicolay Kurdoglo <kolya.kurdoglo.182@gmail.com>
2022-09-22 22:50:56 +0300

    add  more pages, and fix previous problems

--- src/js/components/header.component.js
+++ src/js/components/header.component.js
@@ -6,14 +6,15 @@ export class HeaderComponent extends Component {
     }
 
     init() {
-        this.$el.addEventListener("click", event => {
-            let clickedEl = event.target;
-            
-            if(clickedEl.classList.contains("header__close") || clickedEl.parentNode.classList.contains("header__close")) 
-                this.$el.querySelector(".header__interaction").classList.remove("opened");
-            
-            if(clickedEl.classList.contains("header__burger-menu") || clickedEl.parentNode.classList.contains("header__burger-menu"))
-                this.$el.querySelector(".header__interaction").classList.add("opened");
-        });
+        const burgerMenu = this.$el.querySelector(".header__burger-menu");
+        const closeMenu = this.$el.querySelector(".header__close");
+
+        burgerMenu.addEventListener("click", () => {
+            this.$el.querySelector(".header__interaction").classList.add("opened");
+        })
+
+        closeMenu.addEventListener("click", () => {
+            this.$el.querySelector(".header__interaction").classList.remove("opened");
+        })
     }
 }
